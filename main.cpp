7ace7d26505dad6cdd13ff0e4c406e16f4a0e5b0
Brendan Gould <brendan.gould314@gmail.com>
2023-09-06 16:59:40 -0600

    Vulkan instace creation

--- main.cpp
+++ main.cpp
@@ -1,36 +1,125 @@
+#include <charconv>
+#include <cstdint>
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <stdexcept>
+#include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+  public:
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+  private:
+	GLFWwindow* m_window;
+	VkInstance m_instance;
+
+	void initWindow() {
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		m_window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan() { createInstance(); }
+
+	void createInstance() {
+		VkApplicationInfo appInfo {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo {};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		// get list of graphics extensions required to use glfw
+		// TODO: what are graphics extensions here? do they belong to GPU?
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		/* for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+		    std::cout << "Required extension: " << glfwExtensions[i] << std::endl;
+		} */
+
+		// get list of extensions supported by vulkan instillation
+		uint32_t supportedExtensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &supportedExtensionCount, nullptr);
+		std::vector<VkExtensionProperties> supportedExtensions(supportedExtensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &supportedExtensionCount,
+		                                       supportedExtensions.data());
+
+		/* std::cout << "available extensions:\n";
+
+		for (const auto& extension : supportedExtensions) {
+		    std::cout << '\t' << extension.extensionName << '\n';
+		} */
+
+		// check that required extensions are all supported
+		for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+			bool extensionSupported = false;
+
+			for (const auto& supportedExtension : supportedExtensions) {
+				if (strcmp(glfwExtensions[i], supportedExtension.extensionName) != 0) {
+					extensionSupported = true;
+				}
+			}
+
+			if (!extensionSupported) {
+				throw std::runtime_error("Required extension not supported!");
+			}
+		}
+
+		// create vulkan instance
+		if (vkCreateInstance(&createInfo, nullptr, &m_instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create Vulkan instance!");
+		}
+	}
+
+	void mainLoop() {
+		while (!glfwWindowShouldClose(m_window)) {
+			glfwPollEvents();
+		}
+	}
+
+	void cleanup() {
+		vkDestroyInstance(m_instance, nullptr);
+
+		glfwDestroyWindow(m_window);
+		glfwTerminate();
+	}
+};
 
 int main() {
-    glfwInit();
+	HelloTriangleApplication app;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+	try {
+		app.run();
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    return 0;
+	return EXIT_SUCCESS;
 }
-
