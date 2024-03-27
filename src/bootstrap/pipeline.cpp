9c4ee142b5a6018d948a227a87b350034d8a148b
Brendan Gould <brendan.gould314@gmail.com>
2024-03-26 22:29:42 -0600

    Enable transparency

--- src/bootstrap/pipeline.cpp
+++ src/bootstrap/pipeline.cpp
@@ -561,7 +561,7 @@ PipelineConfigInfo VulkanPipeline::defaultPipelineConfigInfo() {
 	configInfo.colorBlendAttachment.colorWriteMask =
 		VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
 		VK_COLOR_COMPONENT_A_BIT;
-	configInfo.colorBlendAttachment.blendEnable = VK_FALSE;
+	configInfo.colorBlendAttachment.blendEnable = VK_TRUE;
 	configInfo.colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA; // Optional
 	configInfo.colorBlendAttachment.dstColorBlendFactor =
 		VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;                                    // Optional
