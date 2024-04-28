c43c118ced1748309a106a11c102e6f0572f7f8e
Brendan Gould <brendan.gould314@gmail.com>
2024-04-28 15:45:02 -0600

    Shift scene to place camera at origin

--- src/application/application.cpp
+++ src/application/application.cpp
@@ -20,8 +20,8 @@ Application::Application()
 	: m_window(CreateRef<GLFWWindow>("Vulkan")), m_instance(CreateRef<VulkanInstance>(m_window)),
 	  m_device(CreateRef<VulkanDevice>(m_instance)),
 	  m_renderer(CreateRef<VulkanRenderer>(m_instance, m_device, m_window)),
-	  m_camera(CreateRef<Camera>(glm::radians(45.0f), m_renderer->getAspectRatio(),
-                                 glm::vec3(300.0f, 75.0f, -200.0f), 1.0f, 10000.0f)),
+	  m_camera(CreateRef<Camera>(glm::radians(45.0f), m_renderer->getAspectRatio(), glm::vec3(),
+                                 1.0f, 10000.0f)),
 	  m_camController(CreateRef<CameraController>(m_camera)) {
 	if (s_instance) {
 		throw std::runtime_error("Tried to create multiple application instances");
@@ -34,34 +34,28 @@ void Application::run() {
 	Model mountain(m_device, "res/model/mountain.obj",
 	               TextureLibrary::get()->getTexture(m_device, "res/texture/mountain.png"),
 	               ShaderLibrary::get()->getShader(m_device, "model"));
-
-	Model skybox(m_device, "res/skybox/skybox.obj",
-	             TextureLibrary::get()->getTexture(m_device, "res/skybox/skybox.png"),
-	             ShaderLibrary::get()->getShader(m_device, "skybox"));
-	skybox.getTransform().setScale(glm::vec3(1000.0f, 1000.0f, 1000.0f));
-	skybox.getTransform().setTranslation(glm::vec3(0.0f, -200.0f, 0.0f));
-	skybox.getTransform().rotateAbout(m_camera->getUp(), glm::radians(-90.0f));
+	mountain.getTransform().setTranslation({-300.0f, -75.0f, 200.0f});
 
 	Model cloud(m_device, "res/model/cloud.obj",
 	            TextureLibrary::get()->getTexture(m_device, "res/texture/default.png"),
 	            ShaderLibrary::get()->getShader(m_device, "cloud"));
-	cloud.getTransform().setTranslation(glm::vec3(-100.0f, 120.0f, 250.0f));
+	cloud.getTransform().setTranslation(glm::vec3(-400.0f, 45.0f, 450.0f));
 	cloud.getTransform().setScale(glm::vec3(100.0f, 50.0f, 150.0f));
 	Model cloud2(m_device, "res/model/cloud.obj",
 	             TextureLibrary::get()->getTexture(m_device, "res/texture/default.png"),
 	             ShaderLibrary::get()->getShader(m_device, "cloud"));
-	cloud2.getTransform().setTranslation(glm::vec3(0.0f, 150.0f, 0.0f));
+	cloud2.getTransform().setTranslation(glm::vec3(-300.0f, 75.0f, 200.0f));
 	cloud2.getTransform().setScale(glm::vec3(100.0f, 50.0f, 150.0f));
 
 	CloudSettings cloudSettings;
 
 	LightSource light;
-	light.pos = glm::vec3(-1000.0f, 400.0f, 400.0f);
+	light.pos = glm::vec3(-1300.0f, 325.0f, 600.0f);
 	light.color = glm::vec3(0.988f, 0.415f, 0.227f);
 	light.ambientStrength = 0.1f;
 	light.diffuseStrength = 25.0f;
 
-	m_camera->lookAt({0.0f, 60.0f, 100.0f});
+	m_camera->lookAt({-300.0f, -15.0f, 300.0f});
 
 	while (!m_window->shouldClose()) {
 		double newTime = glfwGetTime();
